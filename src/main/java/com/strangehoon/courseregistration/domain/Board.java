f0410029aa1966e54000aa64e907823b0168ca54
Sanghoon Lee <strangehoon2@gmail.com>
2022-12-26 20:39:54 +0900

    Refactor : Board 클래스 속성명 변경

--- src/main/java/com/strangehoon/courseregistration/domain/Board.java
+++ src/main/java/com/strangehoon/courseregistration/domain/Board.java
@@ -5,17 +5,14 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Board extends BaseEntityByBoard {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "board_id")
     private Long id;
 
     private String author;
